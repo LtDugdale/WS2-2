b5256d68398e2cab5060fb206e7980c70d8b9d25
LtDugdale <mnt_xion@hotmail.com>
2017-01-23 12:15:10 +0000

    testing from surface

--- Worksheet2.java
+++ Worksheet2.java
@@ -1,15 +1,22 @@
 import java.io.BufferedWriter;
 import java.io.FileWriter;
 
-/** @author
- *
+/**
+ * @author Laurie Dugdale
  * This class contains the solution for Worksheet2
  */
 
 public class Worksheet2 implements Worksheet2Interface {
 
-    // Exercise 1
-
+    /**
+     * Exercise 1: Negate a tree (5%)
+     *
+     * Given a tree of integers a, write a method that returns a new tree containing all the elements of a with
+     * their sign negated, i.e., positive integers become negative and negative integers become positive.
+     *
+     * @param t
+     * @return
+     */
     public static Tree negateAll(Tree t) {
 
         if (t.isEmpty()) {
@@ -19,8 +26,16 @@ public class Worksheet2 implements Worksheet2Interface {
         }
     }
 
-    // Exercise 2
 
+    /**
+     * Exercise 2: Mirror image (10%)
+     * static Tree mirror(Tree a)
+     * Given a tree a, construct and return a tree that is the mirror image of a along the left-right axis.
+     * The tree on the left is the mirror image of the tree on the right, and vice versa.
+     *
+     * @param t
+     * @return
+     */
     public static Tree mirror(Tree t) {
 
         if (t.isEmpty()) {
@@ -30,8 +45,18 @@ public class Worksheet2 implements Worksheet2Interface {
         }
     }
 
-    // Exercise 3
 
+    /**
+     * Exercise 3: Postorder traversal (10%)
+     *
+     * Given a tree a, produce and return a list containing the values in a by trvaersing the nodes in postorder,
+     * i.e., for every node, all the values in the left subtree should be listed first, then all the values in the right
+     * subtree and then finally the value in the node itself.
+     * Hint: Recall the method for inorder traversal done in the Lecture.
+     *
+     * @param t
+     * @return
+     */
     public static List postorder(Tree t) {
 
         if (t.isEmpty()) {
@@ -42,12 +67,19 @@ public class Worksheet2 implements Worksheet2Interface {
         }
     }
 
-    // Exercise 4
-
+    /**
+     * Exercise 4: Check for positive (5%)
+     *
+     * Given a tree of integers a, return a boolean value indicating whether all the values in its nodes are
+     * positive, i.e., ≥ 0.
+     *
+     * @param a
+     * @return
+     */
     public static boolean allPositive(Tree a) {
 
         if (a.isEmpty()) {
-            throw new IllegalStateException("The list is empty");
+            throw new IllegalStateException("The tree is empty");
         } else if ( a.getLeft().isEmpty() || a.getRight().isEmpty()  ){
             return ( a.getValue() > 0 );
         } else if (a.getValue() < 0) {
@@ -57,12 +89,20 @@ public class Worksheet2 implements Worksheet2Interface {
         }
     }
 
-    // Exercise 5
-
+    /**
+     * Exercise 5: Test for the search tree property (10%)
+     *
+     * Given a tree of integers a, write a method that returns a boolean value indicating whether a is a binary
+     * search tree.
+     * Hint: You may need helper functions to write this method. Document any helper functions you define.
+     *
+     * @param a
+     * @return
+     */
     public static boolean isSearchTree(Tree a) {
 
         if (a.isEmpty()) {
-            throw new IllegalStateException("The list is empty");
+            throw new IllegalStateException("The tree is empty");
         } else if ( a.getLeft().isEmpty() || a.getRight().isEmpty()  ){
             return true;
         } else if ( a.getLeft().getValue() > a.getValue() || a.getRight().getValue() < a.getValue() ) {
@@ -72,8 +112,14 @@ public class Worksheet2 implements Worksheet2Interface {
         }
     }
 
-    // Exercise 6
-
+    /**
+     * Exercise 6: Traversing binary search trees (10%)
+     *
+     * Given a binary search tree of integers a, write a method that prints the values stored in it in descending
+     * order. Do this without building a separate list of the values.
+     *
+     * @param a
+     */
     public static void printDescending(Tree a) {
 
         if(a.isEmpty()) {
@@ -85,8 +131,19 @@ public class Worksheet2 implements Worksheet2Interface {
         }
     }
 
-    // Exercise 7 test again
-
+    /**
+     * Exercise 7: Maximum value in a search tree (10%)
+     *
+     * Assuming that the argument tree a is a binary search tree, write an efficient method to find the maximum
+     * value stored in the tree. Your method must not visit and compare all the nodes in the tree. Rather, it it
+     * must traverse at most one path in the tree from the root node. This should work in O(log n) time for a
+     * balanced binary tree.
+     * (Hint: In a binary search tree, all the values in the left subtree of a node are less than or equal to the
+     * value in the node. So, the maximum value can’t be in the left subtree, right? Where can it be?)
+     *
+     * @param a
+     * @return
+     */
     public static int max(Tree a) {
 
         if (a.isEmpty()){
@@ -98,9 +155,42 @@ public class Worksheet2 implements Worksheet2Interface {
         }
     }
 
-    // Exercise 8
+    /**
+     * Exercise 8: Deleting a value in a search tree (15%)
+     *
+     * Assuming that the argument tree a is a binary search tree, this method must delete the value x from a and
+     * return the resulting tree. The original tree a must not be altered. Rather, you should build a new tree that
+     * contains all the values of a except for one copy of x. The resulting tree must be again a binary search
+     * tree.
+     * Your algorithm must take time proportional to the height of the tree, which is normally O(log n).
+     * (Hint: As discussed in Lecture, the node of x may have two subtrees. In that case, the node cannot be
+     * simply deleted. Rather, you need to replace x in that node with the maximum value of the left subtree.)
+     *
+     * @param x
+     * @param a
+     * @return Tree with specified element removed if element cannot be found tree is not changed.
+     */
     public static Tree delete(int x, Tree a) {
-        return a;
+
+        if (a.isEmpty()) {
+            return new Tree();
+        } else if ( x > a.getValue() ) {
+            return new Tree(a.getValue(), a.getLeft(), delete(x, a.getRight()));
+        } else if ( x < a.getValue() ) {
+            return new Tree(a.getValue(), delete(x, a.getLeft()), a.getRight());
+        } else {
+            if( !a.getRight().isEmpty() && !a.getLeft().isEmpty()) {
+                int M = max(a.getLeft());
+                return new Tree(M, delete(M, a.getLeft()), a.getRight());
+            } else if (a.getLeft().isEmpty() && a.getRight().isEmpty()){
+                return delete(x, new Tree());
+            } else if (a.getLeft().isEmpty()){
+                return new Tree(a.getRight().getValue(), new Tree(), delete(x, a.getRight()));
+            } else  {
+                return new Tree(a.getLeft().getValue(), delete(x, a.getLeft()), new Tree());
+            }
+        }
+
     }
 
 
@@ -136,6 +226,7 @@ public class Worksheet2 implements Worksheet2Interface {
                 new Tree(8, new Tree(7, new Tree(), new Tree()),
                         new Tree(9, new Tree(), new Tree())));
 
+
 //        System.out.println(negateAll(t));
 //        System.out.println(t);
 //        System.out.println(mirror(t));
@@ -143,7 +234,8 @@ public class Worksheet2 implements Worksheet2Interface {
 //        System.out.println(allPositive(t));
 //        System.out.println(isSearchTree(t));
 //        printDescending(t);
-        System.out.println(max(t));
+//        System.out.println(max(t));
+        System.out.println(delete(4, t));
 
 
         }
